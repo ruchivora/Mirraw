8f4c7a63fc5f84ec54a0ffba6bc34e90776053ad
Ruchi1616 <ruchi.vora@mirraw.com>
2021-02-21 18:01:44 +0530

    console in branch 1

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-function showMenu()
-{
-	var menuBox = document.getElementsByClassName("header")[0] ;
+function showMenu() {
+  var menuBox = document.getElementsByClassName("header")[0];
 
-	if( menuBox.style.display == "none")
-			menuBox.style.display = "block" ;
-	else
-		 menuBox.style.display = "none" ;
+  if (menuBox.style.display == "none") menuBox.style.display = "block";
+  else menuBox.style.display = "none";
 }
 
-
+console.log("from branch A");
